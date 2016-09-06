d677a731a4ef6dd9b1938a97db0146f47465e355
Hemanth Reddy <hemanth.sannareddy@wavemaker.com>
2016-09-06 16:22:03 +0530

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "defaultLanguage" : "en",
+  "displayName" : "newm",
+  "homePage" : "Main",
+  "name" : "newm",
+  "platformType" : "MOBILE",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
